18068754f94e55a2a43275598a58a5b0cbfa72f5
Matea <mateamatkovic88@gmail.com>
2025-11-26 22:42:33 +0100

    proba

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center q-pa-md column">
+    <h1>Proba ishod 2</h1>
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
